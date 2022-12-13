4f276d2bccfd10501a9bcc27b82456b9d2fbd7a5
mosesibnmoses <mosesibnmoses@gmail.com>
2022-12-13 16:44:55 -0500

    Day1
    part1

---
+++ Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static int Main(string[] args)
+    {
+        int counter = 0;
+        int cursum = 0;
+        int curmax = 0;
+        foreach (string line in File.ReadLines("input.txt"))
+        {
+            if (line=="")
+            {
+                if (cursum>curmax){
+                    curmax=cursum;
+                    cursum=0;
+                    counter++;
+                }
+                else{
+                    cursum=0;
+                    counter++;
+                }
+            }
+            else{
+                int int_line=Int32.Parse(line);
+                cursum=cursum+int_line;
+            }
+            
+        }
+        Console.Write(curmax);
+        return curmax;
+    }
+}
